79299db79475842475d4e777c7d54ada1707087d
Maxime.jn <114229321+Maxime-jn@users.noreply.github.com>
2025-03-14 13:04:59 +0100

    Add files via upload

---
+++ Accueil.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BriseTête</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <div><a href="Accueil.php">BriseTête<a></div>
+
+        <nav>
+
+
+        </nav>
+        
+        <div><a href="formConnexion.php">Connexion</a></div>
+    </header>
+
+
+
+
+    <main>
+        <div class="classement concours-quiz">
+            <h2>classement concours quiz</h2>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div class="buttons">
+            <a href="menuSeul.php"><button>Seul</button></a>
+            <a href="hubConcours.php"><button>Mode Concour</button></a>
+        </div>
+        <div class="classement concours-casse-tete">
+            <h2>classement concours casse-tête</h2>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </main>
+
+
+
+
+    <footer>
+        Footer
+    </footer>
+</body>
+
+</html>
