e8b6680aca69b7475e2c2db80233e5d5230bc487
Timo74123 <Timoleon.hd@eduge.ch>
2025-03-14 13:08:43 +0100

    Update formConnexion.php

--- formConnexion.php
+++ formConnexion.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BriseTête - Connexion</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <header>
-        <div class="header-left">BriseTête</div>
-        <div class="header-right">connexion</div>
+        <div><a href="Accueil.php">BriseTête<a></div>
+        <div><a href="formConnexion.php">Connexion</a></div>
     </header>
     <main>
-        <div class="login-container">
-            <div class="tabs">
-                <a href="formConnexion.php"><button class="active-tab">Connexion</button></a>
-                <button>Inscription</button>
+        <div>
+            <div>
+                <a href="formConnexion.php"><button>Connexion</button></a>
+                <a href="formInscription.php"><button>Inscription</button></a>
             </div>
             <form>
                 <label for="pseudo">Pseudo</label>
                 <input type="text" id="pseudo" name="pseudo" placeholder="Entrez votre pseudo">
-                
+
                 <label for="password">Mot de passe</label>
                 <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe">
-                
+
                 <button type="submit" class="login-button">Se connecter</button>
             </form>
         </div>
@@ -32,4 +34,5 @@
         Footer
     </footer>
 </body>
+
 </html>
